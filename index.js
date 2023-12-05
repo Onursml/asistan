fcf9e0cf173530598efb99736598b7a7955d0bab
Onursml <65236833+Onursml@users.noreply.github.com>
2023-12-06 01:35:26 +0300

    asdsad
    sadsad

---
+++ index.js
@@ -0,0 +1,40 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client,LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+const user =[
+    {ad:"onur"},
+    {ad:"halil"},
+    {ad:"zeynep"}
+]
+client.on('message_create', (msg) => {
+    
+    // Fired on all message creations, including your own
+    if (msg.fromMe) {
+
+        if (msg.body == 'TEST') {
+            // Send a new message as a reply to the current one
+            msg.reply('TEST ÇALIŞIYOR');
+        
+        }
+      else if (msg.body == 'TEST2') {
+        user.forEach(user => client.sendMessage(msg.from, user.ad))
+       }
+     
+  
+}});
+
+client.initialize();
+ 
